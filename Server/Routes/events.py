1633f11df0e9a8b129932aa6209201f51818e883
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-13 14:57:26 +0200

    Update events.py

--- Server/Routes/events.py
+++ Server/Routes/events.py
@@ -1,12 +1,12 @@
-from app import app
-from Routes import connection
 from flask import Response, Blueprint
+import QueryBuilder
 
 events_bp = Blueprint('events', __name__)
+qb = QueryBuilder.QueryBuilder()
 
 @events_bp.route("/events", methods=["GET"])
 def get_all_events():
-    result = connection.get_all_json("special_events")
+    result = qb.select("special_events").execute()
     if not result:
         return Response(status=404)
     else:
@@ -14,7 +14,7 @@ def get_all_events():
 
 @events_bp.route("/events/<int:id>", methods=["GET"])
 def get_event_by_id(id):
-    result = connection.get_by_id_json("special_events",id)
+    result = qb.select("special_events").where(f"id = {id}").execute()
     if not result:
         return Response(status=404)
     else:
