53733059c6cb835dcff35f61a243a8808bf55db7
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-13 14:56:56 +0200

    Update connection.py

--- Server/Routes/connection.py
+++ Server/Routes/connection.py
@@ -12,30 +12,4 @@ conn = snowflake.connector.connect(
     database=os.environ.get("SNOWFLAKE_DATABASE"),
     schema=os.environ.get("SNOWFLAKE_SCHEMA"),
 )
-
 curs = conn.cursor()
-
-def get_all_json(entity):
-    try:
-        query = f"select * from {entity}"
-        curs.execute(query)
-        column_names = [desc[0] for desc in curs.description]
-        results = curs.fetchall()
-        json_results = [dict(zip(column_names, row)) for row in results]
-        return json_results
-    except:
-      print("An exception occurred")
-
-def get_by_id_json(entity, id):
-    try:
-        curs.execute(f"select * from {entity} where id = {id}")
-        column_names = [desc[0] for desc in curs.description]
-        results = curs.fetchall()
-        json_results = [dict(zip(column_names, row)) for row in results]
-        return json_results
-    except:
-      print("An exception occurred")
-
-def insert_query(query):
-    curs.execute(query)
-    conn.commit()
