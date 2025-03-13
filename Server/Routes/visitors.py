5733ecdd95c5b6898ad627c9f4dfda7872bcf35f
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-13 14:58:25 +0200

    Update visitors.py

--- Server/Routes/visitors.py
+++ Server/Routes/visitors.py
@@ -2,12 +2,14 @@ from app import app
 from flask import Response, Blueprint
 from flask import request
 from Routes import connection
+import QueryBuilder
 
+qb = QueryBuilder.QueryBuilder()
 visitors_bp = Blueprint('visitors', __name__)
 
 @visitors_bp.route("/visitors", methods=["GET"])
 def get_all_visitors():
-    result = connection.get_all_json("visitors;")
+    result = qb.select("visitors").execute()
     if not result:
         return Response(status=404)
     else:
@@ -15,7 +17,7 @@ def get_all_visitors():
 
 @visitors_bp.route("/visitors/<int:id>", methods=["GET"])
 def get_visitor_by_id(id):
-    result = connection.get_by_id_json("visitors",id)
+    result = qb.select("visitors").where(f"id = {id}").execute()
     if not result:
         return Response(status=404)
     else:
@@ -29,8 +31,7 @@ def create_visitor():
         name = request_data["NAME"]
         email = request_data["EMAIL"]
         phone = request_data["PHONE"]
-        query = f"insert into visitors(name, email, phone) values ('{name}', '{email}', '{phone}')"
-        connection.insert_query(query)
+        qb.insert("visitors", ['NAME', 'EMAIL', 'PHONE'], [name, email, phone]).execute()
         return Response(status=201)
     except:
         print("An exception occurred when creating visitor")
@@ -39,7 +40,7 @@ def create_visitor():
 @visitors_bp.route("/visitors/<int:id>", methods=["PUT"])
 def update_visitor_by_id(id):
     #find the entity first
-    result = connection.get_by_id_json("visitors",id)
+    result = qb.select("visitors").where(f"id = {id}").execute()
     if not result:
         return Response(status=404) #entity doesnt exist, cant be updated
     else:
@@ -47,16 +48,16 @@ def update_visitor_by_id(id):
         name = request_data["NAME"]
         email = request_data["EMAIL"]
         phone = request_data["PHONE"]
-        query = f"update visitors set name = '{name}', email = '{email}', phone = '{phone}' where id = {id}"
-        connection.insert_query(query)
+        (qb.update("visitors", ['NAME', 'EMAIL', 'PHONE'], [f"\'{name}\'", f"\'{email}\'", f"\'{phone}\'"])
+         .where(f"id = {id}").print_query()
+         .execute())
         return Response(status=200)
 
 @visitors_bp.route("/visitors/<int:id>", methods=["DELETE"])
 def delete_visitor_by_id(id):
-    result = connection.get_by_id_json("visitors",id)
+    result = qb.select("visitors").where(f"id = {id}").execute()
     if not result:
         return Response(status=404)
     else:
-        query = f"delete from visitors where id = {id}"
-        connection.insert_query(query)
+        qb.delete("visitors").where(f"id = {id}").execute()
         return Response(status=200)
