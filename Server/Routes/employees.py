7cca5c29022ae0f711753284ab5fe44122eab0ed
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-13 14:57:14 +0200

    Update employees.py

--- Server/Routes/employees.py
+++ Server/Routes/employees.py
@@ -1,12 +1,12 @@
-from app import app
-from Routes import connection
 from flask import Response, Blueprint
+import QueryBuilder
 
 employees_bp = Blueprint('employees', __name__)
+qb = QueryBuilder.QueryBuilder()
 
 @employees_bp.route("/employees", methods=["GET"])
 def get_all_employees():
-    result = connection.get_all_json("employees")
+    result = qb.select("employees").execute()
     if not result:
         return Response(status=404)
     else:
@@ -14,7 +14,7 @@ def get_all_employees():
 
 @employees_bp.route("/employees/<int:id>", methods=["GET"])
 def get_employee_by_id(id):
-    result = connection.get_by_id_json("employees", id)
+    result = qb.select("employees").where(f"id = {id}").execute()
     if not result:
         return Response(status=404)
     else:
