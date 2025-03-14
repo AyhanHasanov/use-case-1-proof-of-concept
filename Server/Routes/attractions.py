c194b8b82bf2b76af8f89d03831b29c62b0f9cef
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-14 15:52:26 +0200

    Update attractions.py

--- Server/Routes/attractions.py
+++ Server/Routes/attractions.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask import Response, Blueprint
 import QueryBuilder
 
@@ -6,16 +7,36 @@ qb = QueryBuilder.QueryBuilder()
 
 @attractions_bp.route("/attractions", methods=["GET"])
 def get_all_attractions():
-    result = qb.select("attractions").execute()
-    if not result:
-        return Response(status=404)
-    else:
-        return result
+    try:
+        status = request.args.get('status')
+        type_ = request.args.get('type')
+
+        query = qb.select("attractions")
+        filters = []
+        if status:
+            filters.append(f"status = \'{status}\'")
+        if type_:
+            filters.append(f"type = \'{type_}\'")
+
+        if filters:
+            query = query.where(" AND ".join(filters))
+
+        result = query.execute()
+
+        if not result:
+            return Response(status=404)
+        else:
+            return result
+    except:
+        return Response(status=400)
 
 @attractions_bp.route("/attractions/<int:id>", methods=["GET"])
 def get_attraction_by_id(id):
-    result = qb.select("attractions").where(f"id = {id}").execute()
-    if not result:
-        return Response(status=404)
-    else:
-        return result
+    try:
+        result = qb.select("attractions").where(f"id = {id}").execute()
+        if not result:
+            return Response(status=404)
+        else:
+            return result
+    except:
+        return Response(status=400)
