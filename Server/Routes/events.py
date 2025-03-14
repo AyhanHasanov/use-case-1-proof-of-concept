1e5d2830c35dc478f30cf5e326853a611670cb13
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-14 15:52:55 +0200

    Update events.py

--- Server/Routes/events.py
+++ Server/Routes/events.py
@@ -1,4 +1,5 @@
 from flask import Response, Blueprint
+from flask import request
 import QueryBuilder
 
 events_bp = Blueprint('events', __name__)
@@ -6,16 +7,29 @@ qb = QueryBuilder.QueryBuilder()
 
 @events_bp.route("/events", methods=["GET"])
 def get_all_events():
-    result = qb.select("special_events").execute()
-    if not result:
-        return Response(status=404)
-    else:
-        return result
+    try:
+        type_ = request.args.get("type")
+        query = qb.select("special_events")
+
+        if type_:
+            query = query.where(f"type = \'{type}\'")
+
+        result = query.execute()
+
+        if not result:
+            return Response(status=404)
+        else:
+            return result
+    except:
+        return Response(status=400)
 
 @events_bp.route("/events/<int:id>", methods=["GET"])
 def get_event_by_id(id):
-    result = qb.select("special_events").where(f"id = {id}").execute()
-    if not result:
-        return Response(status=404)
-    else:
-        return result
+    try:
+        result = qb.select("special_events").where(f"id = {id}").execute()
+        if not result:
+            return Response(status=404)
+        else:
+            return result
+    except:
+        return Response(status=400)
