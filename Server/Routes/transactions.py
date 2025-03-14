5e7132f5b88a800bacd0ca2f962018f76f416c8d
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-14 15:53:54 +0200

    Update transactions.py

--- Server/Routes/transactions.py
+++ Server/Routes/transactions.py
@@ -1,5 +1,5 @@
 from flask import Response, Blueprint
-from Routes import connection
+from flask import request
 import QueryBuilder
 
 transactions_bp = Blueprint('transactions', __name__)
@@ -7,16 +7,44 @@ qb = QueryBuilder.QueryBuilder()
 
 @transactions_bp.route("/transactions", methods=["GET"])
 def get_all_transactions():
-    result = qb.select("transactions").execute()
-    if not result:
-        return Response(status=404)
-    else:
-        return result
+    try:
+        type_ = request.args.get("type")
+        sale_id = request.args.get("sale_id")
+        ticket_id = request.args.get("ticket_id")
+        payroll_id = request.args.get("payroll_id")
+        maintanence_id = request.args.get("maintanence_id")
+
+        query = qb.select("transactions")
+        filters = []
+        if type_:
+            filters.append(f"type = '{type_}'")
+        if sale_id:
+            filters.append(f"sale_id = {sale_id}")
+        if ticket_id:
+            filters.append(f"ticket_id = {ticket_id}")
+        if payroll_id:
+            filters.append(f"payroll_id = {payroll_id}")
+        if maintanence_id:
+            filters.append(f"maintanence_id = {maintanence_id}")
+
+        if filters:
+            query = query.where(" AND ".join(filters))
+
+        result = query.execute()
+        if not result:
+            return Response(status=404)
+        else:
+            return result
+    except:
+        return Response(status=400)
 
 @transactions_bp.route("/transactions/<int:id>", methods=["GET"])
 def get_transaction_by_id(id):
-    result = qb.select("transactions").where(f"id = {id}").execute()
-    if not result:
-        return Response(status=404)
-    else:
-        return result
+    try:
+        result = qb.select("transactions").where(f"id = {id}").execute()
+        if not result:
+            return Response(status=404)
+        else:
+            return result
+    except:
+        return Response(status=400)
