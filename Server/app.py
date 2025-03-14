4f318d9feb4ed007876275d9123ad51454c565b7
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-14 15:51:42 +0200

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -12,6 +12,7 @@ def create_app():
     from Routes.tickets import tickets_bp
     from Routes.transactions import transactions_bp
     from Routes.visitors import visitors_bp
+    from Routes.products import products_bp
 
     app.register_blueprint(attractions_bp)
     app.register_blueprint(employees_bp)
@@ -20,16 +21,11 @@ def create_app():
     app.register_blueprint(tickets_bp)
     app.register_blueprint(transactions_bp)
     app.register_blueprint(visitors_bp)
+    app.register_blueprint(products_bp)
     return app
 
 
 app = Flask(__name__)
-
-#--__name__ -> contains current's file name
-# END POINT -> url address with a function attached to it
-#localhost is the loopback ip of the machine -> 127.0.0.1:port
-#routes/endpoints; to access localhost:5000/nameoffunc ->
-# 127.0.0.1:5000/hello returns 'Hello, world'
 
 if __name__ == '__main__':
     app = create_app()
