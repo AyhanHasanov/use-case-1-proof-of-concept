8f86bf35dc637750d8bdabb7967ba1061ef59b8a
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-19 16:42:17 +0200

    Update employees.py

--- Server/Routes/employees.py
+++ Server/Routes/employees.py
@@ -1,41 +1,32 @@
 from flask import request
 from flask import Response, Blueprint
-import QueryBuilder
+from QueryBuilder import QueryBuilder
 
 employees_bp = Blueprint('employees', __name__)
-qb = QueryBuilder.QueryBuilder()
+qb = QueryBuilder()
 
 @employees_bp.route("/employees", methods=["GET"])
 def get_all_employees():
     try:
         job_id = request.args.get('job_id')
         dep_id = request.args.get('dep_id')
-        query = qb.select("employees")
 
-        filters = []
-        if job_id:
-            filters.append(f"job_id = {job_id}")
-        if dep_id:
-            filters.append(f"dep_id = {dep_id}")
+        result = qb.select("employees").where("job_id =", job_id).and_where("department_id =", dep_id).execute()
 
-        if filters:
-            query = query.where(" AND ".join(filters))
-
-        result = query.execute()
         if not result:
             return Response(status=404)
-        else:
-            return result
+
+        return result
     except:
         return Response(status=400)
 
 @employees_bp.route("/employees/<int:id>", methods=["GET"])
 def get_employee_by_id(id):
     try:
-        result = qb.select("employees").where(f"id = {id}").execute()
+        result = qb.select("employees").where(f"id =", id).execute()
         if not result:
             return Response(status=404)
-        else:
-            return result
+
+        return result
     except:
         return Response(status=400)
