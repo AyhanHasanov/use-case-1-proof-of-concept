d399819aae5f880876453c7105712d51a5b3089f
Айхан Хасанов <98754462+AyhanHasanov@users.noreply.github.com>
2025-03-19 16:43:18 +0200

    Update tickets.py

--- Server/Routes/tickets.py
+++ Server/Routes/tickets.py
@@ -1,41 +1,31 @@
 from flask import Response, request, Blueprint
-import QueryBuilder
+from QueryBuilder import QueryBuilder
 
 tickets_bp = Blueprint('tickets', __name__)
-qb = QueryBuilder.QueryBuilder()
+qb = QueryBuilder()
 
 @tickets_bp.route("/tickets", methods=["GET"])
 def get_tickets():
     try:
-        type_ = request.args.get("type")
-        status = request.args.get("status")
-        query = qb.select("tickets")
+        ticket_type = request.args.get("type")
+        ticket_status = request.args.get("status")
 
-        filters = []
-        if status:
-            filters.append(f"status = \'{status}\'")
-        if type_:
-            filters.append(f"type = \'{type_}\'")
-
-        if filters:
-            query = query.where(f" AND ".join(filters))
-
-        result = query.execute()
+        result = qb.select("tickets").where("status =", ticket_status).and_where("type =", ticket_type).execute()
         if not result:
             return Response(status=404)
-        else:
-            return result
+
+        return result
     except:
         return Response(status=400)
 
 @tickets_bp.route("/tickets/<int:id>", methods=["GET"])
 def get_ticket(id):
     try:
-        result = qb.select("tickets").where(f"id = {id}").execute()
+        result = qb.select("tickets").where("id =", id).execute()
         if not result:
             return Response(status=404)
-        else:
-            return result
+
+        return result
     except:
         return Response(status=400)
 
@@ -45,11 +35,11 @@ def create_ticket():
         request_data = request.json
         price = request_data["PRICE"]
         promotion_id = request_data["PROMOTION_ID"]
-        status = request_data["STATUS"]
-        type_ = request_data["TYPE"]
+        ticket_status = request_data["STATUS"]
+        ticket_type = request_data["TYPE"]
         validity_end = request_data["VALIDITY_END"]
         validity_start = request_data["VALIDITY_START"]
-        qb.insert("tickets", ['PRICE', 'PROMOTION_ID', 'STATUS', 'TYPE', 'VALIDITY_END', 'VALIDITY_START'], [price, promotion_id, status, type_, validity_start, validity_end]).execute()
+        qb.insert("tickets", ['PRICE', 'PROMOTION_ID', 'STATUS', 'TYPE', 'VALIDITY_END', 'VALIDITY_START'], [price, promotion_id, ticket_status, ticket_type, validity_start, validity_end]).execute()
         return Response("Ticket successfully created and inserted in the database.", 201)
     except:
         print("An exception occurred when creating ticket")
